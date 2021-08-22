e30ba55439cf6121e9fd450110f9965ffc1d025a
Lucas Matheus Nunes Pereira <86432484+Lukiticas@users.noreply.github.com>
2021-08-22 14:12:29 -0300

    Update Fizzbuzz.py

--- Fizzbuzz.py
+++ Fizzbuzz.py
@@ -1,1 +1,14 @@
-
+def fizz_buzz_maker():
+    for i in range(1, 101):
+        st = ''
+        if not i % 3 and not i % 5:
+            st += 'FizzBuzz'
+        elif not i % 3:
+            st += 'fizz'
+        elif not i % 5:
+            st += 'buzz'
+        else:
+            st = i
+        print(st) 
+        
+fizz_buzz_maker()
